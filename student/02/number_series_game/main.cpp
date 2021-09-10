aa370a30899ba2248296bfc6d1c3e22e399c41ed
Minttu Niiranen <minttu.niiranen@tuni.fi>
2021-09-10 12:38:50 +0300

    Lisätty myös 7:llä jaollisten lukujen käsittely

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -7,7 +7,13 @@ int main()
     std::cout << "How many numbers would you like to have? ";
     std::cin >> numbers;
     for (int number = 1; number <= numbers; ++number){
-        if (number % 3 == 0){
+        if (number % 7 == 0 and number % 3 == 0){
+            std::cout << "zip boing" << std::endl;
+        }
+        else if (number % 7 == 0){
+            std::cout << "boing" << std::endl;
+        }
+        else if (number % 3 == 0){
             std::cout << "zip" << std::endl;
         }
         else{
