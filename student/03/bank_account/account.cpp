4a5c3c2124fc051f5ee18cbf5337499c94f02300
Minttu Niiranen <minttu.niiranen@tuni.fi>
2021-09-19 13:55:17 +0300

    Account luokan alku ja print metodin toteutus

---
+++ student/03/bank_account/account.cpp
@@ -0,0 +1,54 @@
+#include "account.hh"
+#include <iostream>
+
+Account::Account(const std::string& owner, bool has_credit):
+    owner_(owner), has_credit_(has_credit), balance_(0), iban_("")
+{
+    generate_iban();
+}
+
+void Account::print() const
+{
+    std::cout << owner_ << " : " << iban_ << " : " << balance_ << std::endl;
+}
+
+void Account::set_credit_limit(int credit_limit)
+{
+
+}
+
+void Account::save_money(int save)
+{
+
+}
+
+void Account::take_money(int take)
+{
+
+}
+
+void Account::transfer_to(const Account &savings_account, int transfer)
+{
+
+}
+
+// Setting initial value for the static attribute running_number_
+int Account::running_number_ = 0;
+
+void Account::generate_iban()
+{
+    ++running_number_;
+    std::string suffix = "";
+    if(running_number_ < 10)
+    {
+        suffix.append("0");
+    }
+    else if(running_number_ > 99)
+    {
+        std::cout << "Too many accounts" << std::endl;
+    }
+    suffix.append(std::to_string(running_number_));
+
+    iban_ = "FI00 1234 ";
+    iban_.append(suffix);
+}
---
+++ student/03/bank_account/account.hh
@@ -0,0 +1,41 @@
+#ifndef ACCOUNT_HH
+#define ACCOUNT_HH
+
+#include <string>
+
+class Account
+{
+public:
+    // Constructor
+    Account(const std::string& owner, bool has_credit = false);
+
+    // More methods
+
+    void print() const;
+    void set_credit_limit(int credit_limit);
+    void save_money(int save);
+    void take_money(int take);
+    void transfer_to(const Account& savings_account, int transfer);
+
+private:
+    // Generates IBAN (based on running_number_ below).
+    // Allows no more than 99 accounts.
+    void generate_iban();
+
+    // Used for generating IBAN.
+    // Static keyword means that the value of running_number_ is the same for
+    // all Account objects.
+    // In other words, running_number_ is a class-wide attribute, there is
+    // no own copies of it for each object of the class.
+    static int running_number_;
+
+    // More attributes/methods
+
+    const std::string& owner_;
+    bool has_credit_;
+    int balance_;
+    std::string iban_;
+
+};
+
+#endif // ACCOUNT_HH
