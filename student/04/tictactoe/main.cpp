a83827e9951523226040c4a3d165cbd781f86c51
Minttu Niiranen <minttu.niiranen@tuni.fi>
2021-09-25 17:56:45 +0300

    Koordinaattien oikeellisuuden tarkistus

--- student/04/tictactoe/main.cpp
+++ student/04/tictactoe/main.cpp
@@ -34,12 +34,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cctype>
 
 namespace  {
     const int DEFAULT_SIZE = 3;
 }
 
-//Funktio muodostaa halutun kokoisen peliruudukon pelin alkaessa
+// Funktio muodostaa halutun kokoisen peliruudukon pelin alkaessa
 void make_grid(std::vector< std::vector<char> >& grid, size_t n){
 
     for(size_t column = 0; column < n; ++column){
@@ -48,7 +49,7 @@ void make_grid(std::vector< std::vector<char> >& grid, size_t n){
     }
 }
 
-//Funktio tulostaa pelilaudan
+// Funktio tulostaa pelilaudan
 void print_grid(std::vector< std::vector<char> >& grid){
 
     // Tulostetaan x-akselin numerot tauluun
@@ -58,7 +59,7 @@ void print_grid(std::vector< std::vector<char> >& grid){
     }
     std::cout << std::endl;
 
-    //Tulostetaan y-akselin numerot ja loput taulusta riveittäin
+    // Tulostetaan y-akselin numerot ja loput taulusta riveittäin
     for(size_t y = 0; y < grid.size(); ++y){
         std::cout << (y + 1) % 10 << ' ';
         for(size_t x = 0; x < grid.size(); ++x){
@@ -82,6 +83,44 @@ std::string player_in_turn(int player_turn){
     return player_name;
 }
 
+
+// Funktio tarkistaa käyttäjän antaman syötteen oikeellisuuden
+bool check_input(std::string x, std::string y,
+                 std::vector< std::vector<char> >& grid){
+
+    // Koordinaattien pitää olla lukuja
+    if(! std::isdigit(x.at(0)) or ! std::isdigit(y.at(0))){
+        std::cout << "Coordinate outside the board" << std::endl;
+        return false;
+    }
+
+    // Koordinaattien pitää olla pelilaudalla, tai mennä vain yhdellä ali/yli
+    int x1 = std::stoi(x); int y1 = std::stoi(y);
+    size_t x2 = std::stoi(x); size_t y2 = std::stoi(y);
+
+    if(x1 < 0 or x2 > (grid.size() + 1) or y1 < 0 or
+            y2 > (grid.size() + 1)){
+        std::cout << "Coordinate outside the board" << std::endl;
+        return false;
+    }
+    // Koordinaatit ei voi ylittyä ja/tai alittua samaan aikaan
+    if((x1 <= 0 and y1 <= 0) or
+            (x2 >= (grid.size() + 1) and y2 >= (grid.size() + 1))
+            or (x1 <= 0 and y2 >= grid.size()) or
+            (y1 <= 0 and x2 >= grid.size())){
+        std::cout << "Coordinate outside the board" << std::endl;
+        return false;
+    }
+
+    // Peliruudun pitää olla vapaana
+    if(grid.at(std::stoi(x)).at(std::stoi(y)) != '.'){
+        std::cout << "The given place is already occupied" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+
 // Funktio kysyy ja lukee koordinaatit
 void read_coordinates(std::vector< std::vector<char> >& grid){
     std::cout << "Start game:" << std::endl;
@@ -91,17 +130,25 @@ void read_coordinates(std::vector< std::vector<char> >& grid){
     std::string x = "";
     std::string y = "";
 
-    std::cout << "For " << player_in_turn(player_turn) <<
-                 ", enter coordinates: x y>";
-    std::cin >> x;
-    std::cin >> y;
+    while(true){
+        std::cout << "For " << player_in_turn(player_turn) <<
+                     ", enter coordinates: x y> ";
+        std::cin >> x;
 
+        // Tarkistetaan ensin, onko ensimmäinen syöte lopetuskomento
+        if(x == "q"){
+            std::cout << "Why on earth you are giving up the game?" << std::endl;
+            return;
+        }
+        std::cin >> y;
 
+        //Syötettä luetaan kunnes se kelpaa
+        if(check_input(x, y, grid)){
+            break;
+        }
+    }
+    ++player_turn;
 }
-
-
-bool check_input();
-
 
 
 int main()
@@ -109,6 +156,8 @@ int main()
     std::vector< std::vector<char> > grid = {};
     make_grid(grid, DEFAULT_SIZE);
     read_coordinates(grid);
+
+
 
     return 0;
 }
