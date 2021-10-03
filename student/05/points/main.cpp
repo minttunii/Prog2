50fe9d6e80b3de513d5b9e25ef1456c0dfc3016f
Minttu Niiranen <minttu.niiranen@tuni.fi>
2021-10-03 16:43:08 +0300

    Points tehtävä toteutettu

---
+++ student/05/points/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <algorithm>
+
+int main()
+{
+    std::string file = "";
+    std::cout << "Input file: ";
+    std::cin >> file;
+
+    std::ifstream file_in(file);
+
+    if(! file_in){
+        std::cout << "Error! The file " << file << " cannot be opened."
+        << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::map< std::string, int> player_scores;
+    std::string line;
+    size_t sep;
+    std::string name = "";
+    int score = 0;
+
+    while(getline(file_in, line)){
+        sep = line.find(':');
+        name = line.substr(0, sep);
+        score = stoi(line.substr(sep+1, line.length() - (sep + 1)));
+
+        //Jos nimeä ei ole vielä mapissa
+        if(player_scores.find(name) == player_scores.end()){
+            player_scores.insert({name, score});
+        }
+        else{
+            player_scores.at(name) += score;
+        }
+
+    }
+
+    for(auto& pair: player_scores){
+        std::cout<< pair.first << " : " << pair.second << std::endl;
+    }
+
+
+
+    return EXIT_SUCCESS;
+}
