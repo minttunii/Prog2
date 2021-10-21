640aaeca63f25b6c4ca44a6202f4435b9c02256f
Minttu Niiranen <minttu.niiranen@tuni.fi>
2021-10-21 16:29:06 +0300

    Käyttöliittymään lisätty muut virheentarkastelut

--- student/07/education_center2/main.cpp
+++ student/07/education_center2/main.cpp
@@ -167,6 +167,8 @@ bool parse_input_lines(const std::vector<std::string>& lines,
     return true;
 }
 
+// Funktio ottaa komennon ja sen parametrit parametrina, ja tarkastaa parametrien
+//määrän. Jos määrä on virheellinen palautetaan totuusarvo epätosi, muuten tosi.
 bool validate_parameter_count(std::vector<std::string>& parameters,
                               std::string& command){
     if(command == "courses" || command == "COURSES"){
@@ -186,41 +188,147 @@ bool validate_parameter_count(std::vector<std::string>& parameters,
     return true;
 }
 
-void themes_command(std::vector<std::string>& parameters,
-                    CourseCenterMap& courses_by_theme){
-    return;
+// Funktio ottaa parametrina komentoparametrit sekä kurssikeskuksen tiedot.
+// Funktio käy läpi kurssit teemoittain ja etsii kurssia
+// nimeltä. Jos kurssi löytyy funktio palauttaa totuusarvon tosi.
+bool validate_course(std::vector<std::string>& parameters,
+                     CourseCenterMap& courses_by_theme){
+
+   for(const auto& pair : courses_by_theme){
+       std::vector<Course> courses = pair.second;
+       std::string course_name = parameters.at(0);
+
+       auto it = courses.begin();
+       for(; it != courses.end(); ++it){
+           if(it->name == course_name){
+               break;
+           }
+       }
+
+       if(it != courses.end()){
+           return true;;
+       }
+   }
+   return false;
 }
 
-void courses_command(std::vector<std::string>& parameters,
+// Funktio ottaa parametrina komentoparametrit sekä kurssikeskuksen tiedot.
+// Funktio etsii sijaintia kursseista ja palauttaa totuusarvon tosi, jos
+// sijainti löytyy.
+bool validate_location(std::vector<std::string>& parameters,
+                       CourseCenterMap& courses_by_theme){
+
+    for(const auto& pair : courses_by_theme){
+        std::vector<Course> courses = pair.second;
+        std::string location = parameters.at(0);
+
+        auto it = courses.begin();
+        for(; it != courses.end(); ++it){
+            if(it->location == location){
+                break;
+            }
+        }
+
+        if(it != courses.end()){
+            return true;;
+        }
+    }
+    return false;
+}
+
+// Funktio ottaa parametrina komentoparametrit sekä kurssikeskuksen tiedot.
+// Funktio etsii teemaa tiedoista ja palauttaa totuusarvon tosi, jos se löytyy.
+bool validate_theme(std::vector<std::string>& parameters,
+                       CourseCenterMap& courses_by_theme){
+
+    std::string theme = parameters.back();
+    auto it = courses_by_theme.begin();
+    for(; it != courses_by_theme.end(); ++it){
+        if(it->first == theme){
+            return true;
+        }
+    }
+    return false;
+}
+
+// Funktio ottaa parametrina funktion komentoparametrit sekä kurssikeskuksen
+// tiedot. Funktio katsoo, onko teeman alla annetun paikkakunnan kursseja. Jos
+// yhtään kurssia ei löydy, funktio palauttaa epätosi.
+bool is_course_in_location(std::vector<std::string>& parameters,
+                           CourseCenterMap& courses_by_theme){
+
+    std::string location = parameters.at(0);
+    std::string theme = parameters.at(1);
+
+    auto it = courses_by_theme[theme].begin();
+    for(; it != courses_by_theme[theme].end(); ++it){
+        if(it->location == location){
+            break;
+        }
+    }
+    if(it == courses_by_theme[theme].end()){
+        return false;
+    }
+    return true;
+}
+
+void themes_command(std::vector<std::string>& parameters,
+                    CourseCenterMap& courses_by_theme){
+}
+
+bool courses_command(std::vector<std::string>& parameters,
                      CourseCenterMap& courses_by_theme){
-    return;
+    if(!validate_location(parameters, courses_by_theme) &&
+            !validate_theme(parameters, courses_by_theme)){
+        std::cout << "Error: unknown theme" << std::endl;
+        return true;
+    }
+    if(!validate_theme(parameters, courses_by_theme)){
+        std::cout << "Error: unknown theme" << std::endl;
+        return true;
+    }
+    if(!validate_location(parameters, courses_by_theme) ||
+            !is_course_in_location(parameters, courses_by_theme)){
+        std::cout << "Error: unknown location" << std::endl;
+        return true;
+    }
+    return true;
 }
 
 void available_command(std::vector<std::string>& parameters,
                        CourseCenterMap& courses_by_theme){
-    return;
 }
 
 void courses_in_theme_command(std::vector<std::string>& parameters,
                               CourseCenterMap& courses_by_theme){
-    return;
+
+    if(!validate_theme(parameters, courses_by_theme)){
+        std::cout << "Error: unknown theme" << std::endl;
+    }
 }
 
 void courses_in_location_command(std::vector<std::string>& parameters,
                                  CourseCenterMap& courses_by_theme){
-    return;
+    if(!validate_location(parameters, courses_by_theme)){
+        std::cout << "Error: unknown location" << std::endl;
+    }
 }
 
 void favorite_theme_command(std::vector<std::string>& parameters,
                             CourseCenterMap& courses_by_theme){
-    return;
 }
 
 void cancel_command(std::vector<std::string>& parameters,
                     CourseCenterMap& courses_by_theme){
-    return;
+
+    if(!validate_course(parameters, courses_by_theme)){
+       std::cout << "Error: unknown course" << std::endl;
+    }
 }
 
+// Funktio ottaa parametrina komennon ja sen parametrit sekä kurssikeskuksen
+//tiedot. Funktio selvittää komennon ja kutsuu sen funktiota. Funktion paluuarvo
+//on epätosi jos annetaan lopuetuskomento, muuten tosi.
 bool get_command(std::string& command, std::vector<std::string>& parameters,
                  CourseCenterMap& courses_by_theme){
     if(!validate_parameter_count(parameters, command)){
@@ -252,7 +360,6 @@ bool get_command(std::string& command, std::vector<std::string>& parameters,
     }
     else{
         std::cout <<"Error: Unknown command: "<< command <<std::endl;
-        return false;
     }
     return true;
 }
